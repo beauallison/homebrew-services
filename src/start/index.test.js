db535075d6313c8450765efb4711bfd5f2699da8
Beau Allison <beauallison@users.noreply.github.com>
2018-03-10 17:16:39 +0000

    Add error handling to start

--- src/start/index.test.js
+++ src/start/index.test.js
@@ -12,4 +12,13 @@ describe('start()', () => {
     const { status } = await start('kafka');
     expect(status).toEqual('kafka already started');
   });
+
+  it('should return an error for incorrect service', async () => {
+    const error = new Error({
+      status: 'Failed to start Zookeeper',
+      error: 'Error starting Zookeeper',
+    });
+
+    await expect(start('zookeeper')).rejects.toMatchObject(error);
+  });
 });
